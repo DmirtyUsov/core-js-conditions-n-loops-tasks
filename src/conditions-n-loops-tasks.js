97f018a1a4adfde60653f78b46e26097325887c9
Dima Usov <dmitry.usov@yahoo.com>
2024-01-22 14:34:32 +0300

    feat: add convertNumberToString

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -144,8 +144,66 @@ function convertToRomanNumerals(num) {
  *  '10,5'    => 'one zero point five'
  *  '1950.2'  => 'one nine five zero point two'
  */
-function convertNumberToString(/* numberStr */) {
-  throw new Error('Not implemented');
+function convertNumberToString(numberStr) {
+  const getWord = (symbol) => {
+    let word = '';
+    switch (symbol) {
+      case '0':
+        word = 'zero';
+        break;
+      case '1':
+        word = 'one';
+        break;
+      case '2':
+        word = 'two';
+        break;
+      case '3':
+        word = 'three';
+        break;
+      case '4':
+        word = 'four';
+        break;
+      case '5':
+        word = 'five';
+        break;
+      case '6':
+        word = 'six';
+        break;
+      case '7':
+        word = 'seven';
+        break;
+      case '8':
+        word = 'eight';
+        break;
+      case '9':
+        word = 'nine';
+        break;
+      case ',':
+      case '.':
+        word = 'point';
+        break;
+      case '-':
+        word = 'minus';
+        break;
+      default:
+        word = 'unknown';
+    }
+    return word;
+  };
+
+  let index = 0;
+  let digit = numberStr[index];
+  let result = '';
+  while (digit) {
+    result += getWord(digit);
+    index += 1;
+    digit = numberStr[index];
+    if (digit) {
+      result += ' ';
+    }
+  }
+
+  return result;
 }
 
 /**
